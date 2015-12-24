23992b3a347931e3d02fe0b847136a5fdfbf1255
Kaivo Anastetiks <kaivoanastetiks@gmail.com>
2015-12-24 07:46:25 -0500

    Add test index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+var Firebase = require('firebase');
+
+var ref = new Firebase('https://' + process.env.FIREBASE_SUBDOMAIN + '.firebaseio.com');
+ref.authWithCustomToken(process.env.FIREBASE_AUTH, function(error, authData) {
+    if(error) {
+        console.log('Error ', error);
+    }
+    else {
+        console.log('Success ', authData);
+        ref.set({
+            title: 'blabla',
+            author: 'blablabla',
+            localtion: {
+                city: 'blabla',
+                country: 'blablabla'
+            }
+        });
+       process.exit(0);
+    }
+});
+
