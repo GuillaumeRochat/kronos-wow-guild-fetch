6c574cea539730c289fc130c52bafd7516809c37
Kaivo Anastetiks <kaivoanastetiks@gmail.com>
2016-01-09 16:24:12 -0500

    Changes item to activity and include a concept of type for loot or bosskill.

--- test/models/activity.spec.js
+++ test/models/activity.spec.js
@@ -1,11 +1,22 @@
 var expect = require('expect.js');
-var Item = require('_/models/item');
+var Activity = require('_/models/activity');
 
-describe('_/models/item', function() {
-    it('sets the id if it above 0', function() {
-        var intBelow = new Item();
-        var intAbove = new Item();
-        var notInt = new Item();
+describe('_/models/activity', function() {
+    it('sets the type if valid', function() {
+        var valid = new Activity();
+        var notValid = new Activity();
+
+        valid.setType('loot');
+        notValid.setType('death');
+
+        expect(valid.get('type')).to.be('loot');
+        expect(notValid.get('type')).to.be(null);
+    });
+
+    it('sets the id if the activity above 0', function() {
+        var intBelow = new Activity();
+        var intAbove = new Activity();
+        var notInt = new Activity();
 
         intBelow.setId(0);
         intAbove.setId(1);
@@ -17,9 +28,9 @@ describe('_/models/item', function() {
     });
 
     it('sets the character name if not empty string', function() {
-        var emptyString = new Item();
-        var notEmptyString = new Item();
-        var notString = new Item();
+        var emptyString = new Activity();
+        var notEmptyString = new Activity();
+        var notString = new Activity();
 
         emptyString.setCharacterName('');
         notEmptyString.setCharacterName('characterName');
@@ -31,8 +42,8 @@ describe('_/models/item', function() {
     });
 
     it('sets the datime if valid ISO-8601 at UTC time', function() {
-        var iso = new Item();
-        var notIso = new Item();
+        var iso = new Activity();
+        var notIso = new Activity();
 
         iso.setDatetime('1970-01-01T12:12:12Z');
         notIso.setDatetime('70-01-01 12:12:12');
@@ -42,9 +53,10 @@ describe('_/models/item', function() {
     });
 
     it('returns true if no model value is null', function() {
-        var hasNull = new Item();
-        var notHasNull = new Item();
+        var hasNull = new Activity();
+        var notHasNull = new Activity();
 
+        notHasNull.setType('loot');
         notHasNull.setId(1);
         notHasNull.setCharacterName('characterName');
         notHasNull.setDatetime('1970-01-01T00:00:00Z');
