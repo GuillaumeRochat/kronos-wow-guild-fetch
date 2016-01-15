3fa6129b293eadea209236398001ba3803cae3d9
Kaivo Anastetiks <kaivoanastetiks@gmail.com>
2016-01-14 20:54:09 -0500

    Completes the characters fetch.

--- test/fetch/index.spec.js
+++ test/fetch/index.spec.js
@@ -1,11 +1,21 @@
-var Promise = require('bluebird');
 var expect = require('expect.js');
+var nock = require('nock');
+
 var Fetch = require('_/fetch');
+
+before(function() {
+    nock.disableNetConnect();
+});
 
 describe('_/fetch', function() {
     it('returns a Promise when calling Guild', function() {
-        var guild = Fetch.Guild('realm', 'guildName');
-        expect(guild.then).to.be.a('function');
+        nock('http://armory.twinstar.cz')
+            .get('/guild-info.xml')
+            .query({ r: 'realm', gn: 'guildName' })
+            .reply(200, 'ok');
+
+        var characters = Fetch.GetCharacters('realm', 'guildName');
+        expect(characters.then).to.be.a('function');
     });
 });
 
