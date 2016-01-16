23421706d5b7777acac2da0019537876d772021f
Kaivo Anastetiks <kaivoanastetiks@gmail.com>
2016-01-16 13:07:46 -0500

    Completes the profession fetching.

--- test/fetch/professions.spec.js
+++ test/fetch/professions.spec.js
@@ -8,7 +8,6 @@ var Professions = require('_/fetch/professions');
 const URL = 'http://example.com';
 const REALM = 'realm';
 const CHARACTER_NAME = 'characterName';
-const GUILD_NAME = 'guildName';
 const REPLY = `
 <?xml version="1.0" encoding="UTF-8"?>
 <?xml-stylesheet type="text/xsl" href="_layout/character/sheet.xsl"?>
@@ -22,7 +21,7 @@ const REPLY = `
             </professions>
             <skills>
                 <skill id="129" name="First Aid" skill="1"/>
-                <skill id="185" name="Cooking" skill="0"/>
+                <skill id="185" name="Cooking" skill="1"/>
                 <skill id="356" name="Fishing" skill="0"/>
                 <skill id="762" name="Riding" skill="75"/>
             </skills>
@@ -38,7 +37,7 @@ before(function() {
 beforeEach(function() {
     nock(URL)
         .get('/character-sheet.xml')
-        .query({ r: REALM, cn: CHARACTER_NAME, gn: GUILD_NAME })
+        .query({ r: REALM, cn: CHARACTER_NAME })
         .reply(200, REPLY);
 });
 
@@ -47,25 +46,24 @@ afterEach(function() {
 });
 
 describe('_/fetch/professions', function() {
-    it('throws exception if URL, realm, characterName or guildName is not string', function() {
+    it('throws exception if URL, realm or characterName is not string', function() {
         var closure = function() {
-            var professions = new Professions(1, 2, 3, 4);
+            var professions = new Professions(1, 2, 3);
         };
 
         expect(closure).to.throwError();
     });
 
-    it('sets the url, realm, characterName and guildName', function() {
-        var professions = new Professions(URL, REALM, CHARACTER_NAME, GUILD_NAME);
+    it('sets the url, realm and characterName', function() {
+        var professions = new Professions(URL, REALM, CHARACTER_NAME);
 
         expect(professions.url).to.be(URL);
         expect(professions.realm).to.be(REALM);
         expect(professions.characterName).to.be(CHARACTER_NAME);
-        expect(professions.guildName).to.be(GUILD_NAME);
     });
 
     it('calls the fetch function when calling getData', function() {
-        var professions = new Professions(URL, REALM, CHARACTER_NAME, GUILD_NAME);
+        var professions = new Professions(URL, REALM, CHARACTER_NAME);
         var spy = sinon.spy(professions, 'fetch');
 
         return professions.getData().then(function() {
@@ -74,7 +72,7 @@ describe('_/fetch/professions', function() {
     });
 
     it('calls the parse function when calling getData', function() {
-        var professions = new Professions(URL, REALM, CHARACTER_NAME, GUILD_NAME);
+        var professions = new Professions(URL, REALM, CHARACTER_NAME);
         var spy = sinon.spy(professions, 'parse');
 
         return professions.getData().then(function() {
@@ -82,24 +80,16 @@ describe('_/fetch/professions', function() {
         });
     });
 
-    it('replaces the guildName spaces with + signs in getQuery', function() {
-        var professions = new Professions(URL, REALM, CHARACTER_NAME, 'guild name with spaces');
-
-        var query = professions.getQuery();
-
-        expect(query).to.be(URL + '/character-sheet.xml?r=' + REALM + '&cn=' + CHARACTER_NAME + '&gn=guild+name+with+spaces');
-    });
-
     it('replaces the realm spaces with + signs in getQuery', function() {
-        var professions = new Professions(URL, 'realm name with spaces', CHARACTER_NAME, GUILD_NAME);
+        var professions = new Professions(URL, 'realm name with spaces', CHARACTER_NAME);
 
         var query = professions.getQuery();
 
-        expect(query).to.be(URL + '/character-sheet.xml?r=realm+name+with+spaces&cn=' + CHARACTER_NAME + '&gn=' + GUILD_NAME);
+        expect(query).to.be(URL + '/character-sheet.xml?r=realm+name+with+spaces&cn=' + CHARACTER_NAME);
     });
 
     it('performs the request of the query when calling fetch', function() {
-        var professions = new Professions(URL, REALM, CHARACTER_NAME, GUILD_NAME);
+        var professions = new Professions(URL, REALM, CHARACTER_NAME);
 
         return professions.fetch().then(function(response) {
             expect(response).to.be(REPLY);
@@ -107,7 +97,7 @@ describe('_/fetch/professions', function() {
     });
 
     it('returns an array of Profession objects when calling parse', function() {
-        var professions = new Professions(URL, REALM, CHARACTER_NAME, GUILD_NAME);
+        var professions = new Professions(URL, REALM, CHARACTER_NAME);
 
         var professionsData = professions.parse(REPLY);
 
@@ -116,6 +106,57 @@ describe('_/fetch/professions', function() {
         professionsData.forEach(function(profession) {
             expect(profession).to.be.a(Profession);
         });
+    });
+
+    it('sets the primary profession name in lower case from the node in the model', function() {
+        var professions = new Professions(URL, REALM, CHARACTER_NAME);
+
+        var professionsData = professions.parse(REPLY);
+
+        expect(professionsData[0].get('name')).to.be('blacksmithing');
+        expect(professionsData[1].get('name')).to.be('mining');
+    });
+
+    it('sets the secondary profession name in lower case from the node in the model', function() {
+        var professions = new Professions(URL, REALM, CHARACTER_NAME);
+
+        var professionsData = professions.parse(REPLY);
+
+        expect(professionsData[2].get('name')).to.be('first aid');
+        expect(professionsData[3].get('name')).to.be('cooking');
+    });
+
+    it('does not add secondary profession with skill of 0', function() {
+        var professions = new Professions(URL, REALM, CHARACTER_NAME);
+
+        var professionsData = professions.parse(REPLY);
+
+        expect(professionsData).to.have.length(5);
+        professionsData.forEach(function(profession) {
+            expect(profession.get('name')).to.not.be('fishing');
+        });
+    });
+
+    it('sets the character name provided in the search in the model', function() {
+        var professions = new Professions(URL, REALM, CHARACTER_NAME);
+
+        var professionsData = professions.parse(REPLY);
+
+        professionsData.forEach(function(profession) {
+            expect(profession.get('characterName')).to.be(CHARACTER_NAME);
+        });
+    });
+
+    it('sets the profession leve from the node in the model', function() {
+        var professions = new Professions(URL, REALM, CHARACTER_NAME);
+
+        var professionsData = professions.parse(REPLY);
+
+        expect(professionsData[0].get('level')).to.be(300);
+        expect(professionsData[1].get('level')).to.be(300);
+        expect(professionsData[2].get('level')).to.be(1);
+        expect(professionsData[3].get('level')).to.be(1);
+        expect(professionsData[4].get('level')).to.be(75);
     });
 });
 
